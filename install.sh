f27a3bf4b4ba39cc22d9dc96989c28e49a1e9925
Rafal Sladek <rafal.sladek@gmail.com>
2018-06-21 01:45:22 +0200

    add missing libevent-dev

--- install.sh
+++ install.sh
@@ -65,7 +65,7 @@ apt-get update >/dev/null 2>&1
 apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" make software-properties-common \
 build-essential libtool autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev libboost-program-options-dev \
 libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git wget pwgen curl libdb4.8-dev bsdmainutils \
-libdb4.8++-dev libminiupnpc-dev libgmp3-dev ufw automake
+libdb4.8++-dev libminiupnpc-dev libgmp3-dev ufw automake libevent-dev
 clear
 if [ "$?" -gt "0" ];
   then
@@ -76,7 +76,7 @@ if [ "$?" -gt "0" ];
     echo "apt-get update"
     echo "apt install -y make build-essential libtool software-properties-common autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev \
 libboost-program-options-dev libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git pwgen curl libdb4.8-dev \
-bsdmainutils libdb4.8++-dev libminiupnpc-dev libgmp3-dev ufw"
+bsdmainutils libdb4.8++-dev libminiupnpc-dev libgmp3-dev ufw automake libevent-dev"
  exit 1
 fi
 
