14db791d4c81720a58a736a6c064b13804b0bd4b
Rafal Sladek <rafal.sladek@gmail.com>
2018-06-30 03:40:28 +0200

    no email output in cronjob

--- install_arcticstatus_cronjob.sh
+++ install_arcticstatus_cronjob.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-echo "* * * * * /usr/local/bin/arcticstatus > /var/www/html/arcticstatus.json" >> /var/spool/cron/crontabs/root
+echo "* * * * * /usr/local/bin/arcticstatus > /var/www/html/arcticstatus.json >/dev/null 2>&1" >> /var/spool/cron/crontabs/root
 
 sudo crontab -l
+sudo crontab -e
